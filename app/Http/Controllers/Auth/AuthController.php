473a8fe7d1633fe64d6e50f88e7cd6cb9d1f895b
Diego Santana <3c.santana@gmail.com>
2026-03-27 19:47:20 -0300

    feat: Cria o AuthController

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    /**
+     * Exibe a view com o formulário de login
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     *  Faz a autenticação do usuário
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Faz o logout, finalizando a sessão do usuário
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
