d1069abd0f032b69ed6031f865df7e16dd658286
Diego Santana <3c.santana@gmail.com>
2026-03-27 19:47:11 -0300

    feat: Cria o DashboardController

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+/**
+ * Gerencia o acesso ao Dashboard
+ */
+class DashboardController extends Controller
+{
+    /**
+     * Exibe a view do dashboard
+     */
+    public function index()
+    {
+        return 'Bem vindo!';
+    }
+}
