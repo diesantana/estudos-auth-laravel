5e4b345fc14c806bd550520b73ccc9e698c00133
Diego Santana <3c.santana@gmail.com>
2026-03-27 19:46:46 -0300

    feat: Define as rotas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('app');
+// Rotas públicas
+Route::middleware('guest')->group(function () {
+    // Exibe a view de Login
+    Route::get('/login', [AuthController::class, 'create'])->name('login');
+    // Processa o Login
+    Route::post('/login', [AuthController::class, 'store']);
+    // Exibe a view de cadastro
+    Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
+    // Processa o cadastro
+    Route::post('/register', [RegisteredUserController::class, 'store']);
+});
+
+// Rotas protegidas
+Route::middleware('auth')->group(function () {
+    // Apenas redireciona para a rota do dashboard
+    Route::get('/', function () {return redirect()->route('dashboard');});
+    // Logout
+    Route::post('/logout', [AuthController::class, 'destroy'])->name('logout');
+    // Exibe a view Dashboard
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 });
