254e565b336aa1ef70fef00195a21e6aec8bdadd
Diego Santana <3c.santana@gmail.com>
2026-03-27 19:47:39 -0300

    feat: Cria o RegisteredUserController

---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+/**
+ * Lida com o cadastro de usuários
+ */
+class RegisteredUserController extends Controller
+{
+
+    /**
+     * Exibe a view com o formulário de cadastro
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Salva o usuário na base de dados
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+}
