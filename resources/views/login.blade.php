4d610e4bbf87dbe580394b8af9677a5c7f3b8eb5
Diego Santana <3c.santana@gmail.com>
2026-03-29 19:11:22 -0300

    feat: Estiliza o botão do form

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -14,51 +14,49 @@
             @csrf
 
             {{-- título --}}
-            <h1 class="text-[28px] font-semibold text-gray-900 mb-8">Faça login na sua conta</h1>
+            <h1 class="text-[28px] font-semibold text-gray-900">Faça login na sua conta</h1>
 
             {{-- email --}}
             <div class="flex flex-col w-full gap-3 text-slate-700">
                 <label for="email">Email</label>
                 <input type="email" name="email" id="email" placeholder="Digite o seu email"
-                    class="py-3 px-4 rounded-lg border-2 border-[#D0D5DD] border-solid text-[14px] text-gray-400
+                    class="py-3 px-4 rounded-lg border-2 border-[#D0D5DD] border-solid text-sm text-gray-400
                     focus:border-[#D1E9FF] focus:border-2 focus:border-solid focus:outline-none">
             </div>
 
             {{-- password --}}
-            {{-- <div class="flex flex-col w-full gap-3 text-slate-700">
-                <label for="password">Senha</label>
-                <input type="password" name="password" id="password" placeholder="Digite a sua senha"
-                    class="py-3 px-4 rounded-lg border-2 border-[#D0D5DD] border-solid text-[14px] text-gray-400
-                    focus:border-[#D1E9FF] focus:border-2 focus:border-solid focus:outline-none">
-                <a href="#">Esqueci</a>
-            </div> --}}
             <div class="flex flex-col w-full gap-3 text-slate-700">
-                <label for="password">Senha</label>
+                {{-- Container para a label e o link forgot --}}
+                <div class="flex justify-between">
+                    <label for="password">Senha</label>
+                    <a href="#" class="text-blue-600 hover:underline">Esqueci?</a>
+                </div>
 
-                <div class="relative w-full"> {{-- Removido max-w-xs para ocupar a largura total do pai --}}
-
+                {{-- Container para o INPUT --}}
+                <div class="relative w-full">
                     <!-- Div que posiciona o ícone -->
                     <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
                         <x-heroicon-o-eye class="h-5 w-5 text-gray-400" />
                     </div>
 
-                    <!-- O seu input -->
+                    <!-- input -->
                     <input type="password" name="password" id="password" placeholder="Digite a sua senha"
-                        class="block w-full rounded-lg border-2 border-[#D0D5DD] border-solid
-                        py-3 pl-4 text-sm text-gray-900 placeholder:text-gray-400 focus:border-[#D1E9FF]
+                        class="w-full rounded-lg border-2 border-[#D0D5DD] border-solid
+                        py-3 pl-4 text-sm text-gray-400 focus:border-[#D1E9FF]
                         focus:border-2 focus:border-solid focus:outline-none pr-10">
                 </div>
-                <a href="#" class="text-sm self-end">Esqueci</a> {{-- Adicionado self-end para alinhar à direita --}}
             </div>
 
             {{-- BTN --}}
-            <div>
-                <button type="submit">Entrar</button>
+            <div class="w-full">
+                <button type="submit"
+                    class="bg-blue-600 hover:bg-blue-500  w-full p-4 text-gray-50 cursor-pointer
+                    rounded-lg font-semibold shadow-lg">Entrar</button>
             </div>
 
             {{-- LINK CADASTRAR --}}
-            <div>
-                <a href="#">Ainda não tem uma conta? <span>Cadastrar-se</span></a>
+            <div class="text-gray-400 flex justify-center">
+                <p>Ainda não tem uma conta? <a href="#" class="text-blue-600 hover:underline ml-2">Cadastrar-se</a></p>
             </div>
         </form>
         {{-- =================================================================== --}}
