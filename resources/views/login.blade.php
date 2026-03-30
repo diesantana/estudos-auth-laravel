2e81fa005970fc073aef727fea7bf15b3f7b59b1
Diego Santana <3c.santana@gmail.com>
2026-03-30 09:31:19 -0300

    feat: Implementa a exibição de erros na view login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,19 +9,35 @@
         {{-- =================================================================== --}}
         {{-- FORM  --}}
         {{-- =================================================================== --}}
-        <form action=""
+        <form action="/login" method="POST"
             class="bg-white flex justify-center items-center flex-col py-6 md:py-12 px-6 md:px-18 gap-8 w-full max-w-xl shadow-[40px_40px_60px_0px_rgba(228,230,234,0.74)] rounded-[20px]">
             @csrf
 
             {{-- título --}}
             <h1 class="text-[28px] font-semibold text-gray-900">Faça login na sua conta</h1>
 
+            {{-- exibição de erros genéricos --}}
+            @if ($errors->has('credentials'))
+                <div class="p-4 text-sm text-red-800 rounded-lg bg-red-100" role="alert">
+                    <span class="font-medium">Ops!</span>
+                    {{ $errors->first('credentials') }}
+                </div>
+            @endif
+
             {{-- email --}}
             <div class="flex flex-col w-full gap-3 text-slate-700">
                 <label for="email">Email</label>
                 <input type="email" name="email" id="email" placeholder="Digite o seu email"
-                    class="py-3 px-4 rounded-lg border-2 border-[#D0D5DD] border-solid text-sm text-gray-400
-                    focus:border-[#D1E9FF] focus:border-2 focus:border-solid focus:outline-none">
+                    @class([
+                        'py-3 px-4 rounded-lg border-2 border-solid text-sm text-gray-400
+                            focus:border-[#D1E9FF] focus:border-2 focus:border-solid focus:outline-none',
+                        'border-red-400' => $errors->has('email'),
+                        'border-[#D0D5DD]' => !$errors->has('email'),
+                    ])>
+                {{-- Bloco de erros --}}
+                @error('email')
+                    <p class="text-red-600 text-sm">{{ $message }}</p>
+                @enderror
             </div>
 
             {{-- password --}}
@@ -41,10 +57,19 @@
 
                     <!-- input -->
                     <input type="password" name="password" id="password" placeholder="Digite a sua senha"
-                        class="w-full rounded-lg border-2 border-[#D0D5DD] border-solid
-                        py-3 pl-4 text-sm text-gray-400 focus:border-[#D1E9FF]
-                        focus:border-2 focus:border-solid focus:outline-none pr-10">
+                        @class([
+                            'w-full rounded-lg border-2 border-solid
+                                py-3 pl-4 text-sm text-gray-400 focus:border-[#D1E9FF]
+                                focus:border-2 focus:border-solid focus:outline-none pr-10',
+                            'border-red-400' => $errors->has('password'),
+                            'border-[#D0D5DD]' => !$errors->has('password'),
+                        ])>
+
                 </div>
+                {{-- Bloco de erros --}}
+                @error('password')
+                    <p class="text-red-600 text-sm">{{ $message }}</p>
+                @enderror
             </div>
 
             {{-- BTN --}}
@@ -56,7 +81,8 @@
 
             {{-- LINK CADASTRAR --}}
             <div class="text-gray-400 flex justify-center">
-                <p>Ainda não tem uma conta? <a href="{{route('register')}}" class="text-blue-600 hover:underline ml-2">Cadastrar-se</a></p>
+                <p>Ainda não tem uma conta? <a href="{{ route('register') }}"
+                        class="text-blue-600 hover:underline ml-2">Cadastrar-se</a></p>
             </div>
         </form>
         {{-- =================================================================== --}}
