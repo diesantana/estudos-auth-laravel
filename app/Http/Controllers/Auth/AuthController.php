f7cbd5c58879424681e513859c6a1775b8665ff7
Diego Santana <3c.santana@gmail.com>
2026-03-30 09:31:52 -0300

    feat: Implementa o login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -18,9 +21,22 @@ class AuthController extends Controller
     /**
      *  Faz a autenticação do usuário
      */
-    public function store(Request $request)
+    public function store(LoginRequest $request)
     {
-        //
+        // Pega as credenciais validadas pelo LoginRequest
+        $credentials = $request->validated();
+
+        // Tenta autenticar o usuário
+        if (Auth::attempt($credentials)) {
+            // Sucesso! Regenera a sessão
+            $request->session()->regenerate();
+            return redirect()->intended('/dashboard'); // redireciona para o dashboard.
+        }
+
+        // Exibe um erro genérico em caso de falha de autenticação por email ou senha.
+        throw ValidationException::withMessages([
+            'credentials' => 'E-mail ou senha inválidos. Verifique os dados e tente novamente.',
+        ]);
     }
 
     /**
